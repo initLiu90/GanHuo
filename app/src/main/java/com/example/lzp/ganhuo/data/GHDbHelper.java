1182b16854b9b5d7e9ad7e657d8ebda740a6c3e7
initLiu <xianyue_pan@163.com>
2017-03-14 23:40:26 +0800

    添加contentprovider

---
+++ app/src/main/java/com/example/lzp/ganhuo/data/GHDbHelper.java
@@ -0,0 +1,56 @@
+package com.example.lzp.ganhuo.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.lzp.ganhuo.data.today.TodayPersistenceContract;
+
+/**
+ * Created by lzp on 2017/3/14.
+ */
+
+public class GHDbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String DATABASE_NAME = "ganhuo.db";
+
+    private static final String TEXT_TYPE = " TEXT";
+
+    private static final String INTEGER_TYPE = " INTEGER";
+
+    private static final String BOOLEAN_TYPE = " INTEGER";
+
+    private static final String COMMA_SEP = ",";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TodayPersistenceContract.TodayEntry.TABLE_NAME + " (" +
+                    TodayPersistenceContract.TodayEntry._ID + INTEGER_TYPE + " PRIMARY KEY," +
+                    TodayPersistenceContract.TodayEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
+                    TodayPersistenceContract.TodayEntry.COLUMN_NAME_TYPE + TEXT_TYPE + COMMA_SEP +
+                    TodayPersistenceContract.TodayEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+                    TodayPersistenceContract.TodayEntry.COLUMN_NAME_URL + TEXT_TYPE + COMMA_SEP +
+                    TodayPersistenceContract.TodayEntry.COLUMN_NAME_AUTHER + TEXT_TYPE + COMMA_SEP +
+                    TodayPersistenceContract.TodayEntry.COLUMN_NAME_IMAGEURL + TEXT_TYPE + COMMA_SEP +
+                    TodayPersistenceContract.TodayEntry.COLUMN_NAME_DATE + TEXT_TYPE +
+                    " )";
+
+    public GHDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        super.onDowngrade(db, oldVersion, newVersion);
+    }
+}
---
+++ app/src/main/java/com/example/lzp/ganhuo/data/GHProvider.java
@@ -0,0 +1,158 @@
+package com.example.lzp.ganhuo.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.view.menu.MenuAdapter;
+
+import com.example.lzp.ganhuo.BuildConfig;
+import com.example.lzp.ganhuo.data.today.TodayPersistenceContract;
+import com.example.lzp.ganhuo.fragment.today.Today;
+
+/**
+ * Created by lzp on 2017/3/14.
+ */
+
+public class GHProvider extends ContentProvider {
+    private static final int TODAY = 100;
+    private static final int TODAY_ITEM = 101;
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private GHDbHelper mGHDbHelper;
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = BuildConfig.APPLICATION_ID;
+
+        matcher.addURI(authority, "today", TODAY);
+        matcher.addURI(authority, "today/*", TODAY_ITEM);
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mGHDbHelper = new GHDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case TODAY:
+                return "vnd.android.cursor.dir/" + BuildConfig.APPLICATION_ID + "/" + "today";
+            case TODAY_ITEM:
+                return "vnd.android.cursor.item/" + BuildConfig.APPLICATION_ID + "/" + "today";
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        Cursor retCursor;
+        switch (sUriMatcher.match(uri)) {
+            case TODAY:
+                retCursor = mGHDbHelper.getReadableDatabase().query(TodayPersistenceContract.TodayEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            case TODAY_ITEM:
+                String[] where = {uri.getLastPathSegment()};
+                retCursor = mGHDbHelper.getReadableDatabase().query(TodayPersistenceContract.TodayEntry.TABLE_NAME,
+                        projection,
+                        TodayPersistenceContract.TodayEntry.COLUMN_NAME_ENTRY_ID + " = ?",
+                        where,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("unknow uri:" + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        final SQLiteDatabase db = mGHDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case TODAY:
+                Cursor exists = db.query(TodayPersistenceContract.TodayEntry.TABLE_NAME,
+                        new String[]{TodayPersistenceContract.TodayEntry.COLUMN_NAME_ENTRY_ID},
+                        TodayPersistenceContract.TodayEntry.COLUMN_NAME_ENTRY_ID + " = ?",
+                        new String[]{values.getAsString(TodayPersistenceContract.TodayEntry.COLUMN_NAME_ENTRY_ID)},
+                        null,
+                        null,
+                        null);
+                if (exists.moveToLast()) {
+                    long _id = db.update(TodayPersistenceContract.TodayEntry.TABLE_NAME, values, TodayPersistenceContract.TodayEntry.COLUMN_NAME_ENTRY_ID + " = ?",
+                            new String[]{values.getAsString(TodayPersistenceContract.TodayEntry.COLUMN_NAME_ENTRY_ID)});
+                    if (_id > 0) {
+                        returnUri = TodayPersistenceContract.TodayEntry.buildTodayUriWith(_id);
+                    } else {
+                        throw new android.database.SQLException("Failed to insert row into " + uri);
+                    }
+                } else {
+                    long _id = db.insert(TodayPersistenceContract.TodayEntry.TABLE_NAME, null, values);
+                    if (_id > 0) {
+                        returnUri = TodayPersistenceContract.TodayEntry.buildTodayUriWith(_id);
+                    } else {
+                        throw new android.database.SQLException("Failed to insert row into " + uri);
+                    }
+                }
+                exists.close();
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mGHDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsDeleted;
+        switch (match) {
+            case TODAY:
+                rowsDeleted = db.delete(TodayPersistenceContract.TodayEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (selection == null || rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mGHDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsUpdated;
+
+        switch (match) {
+            case TODAY:
+                rowsUpdated = db.update(TodayPersistenceContract.TodayEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+}
